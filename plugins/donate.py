d8aed0f7e19577ccf161612ab53ffd43f397694d
InvisibleSymbol <pettypalkovicsreal@gmail.com>
2021-09-21 17:12:06 +0200

    add proof of ownership for donation address

--- plugins/donate.py
+++ plugins/donate.py
@@ -20,7 +20,7 @@ class Stats(commands.Cog):
   async def donate(self, ctx):
     """Donate to the Bot Developer"""
     embed = Embed()
-    embed.description = "Donation Address: **`0x87FF5B8ccFAeEC77b2B4090FD27b11dA2ED808Fb`**"
+    embed.description = "Donation Address: **`0x87FF5B8ccFAeEC77b2B4090FD27b11dA2ED808Fb`** ([Ownership Proof](https://etherscan.io/verifySig/3414))"
     embed.set_footer(text="Ethereum or Ethereum-based Rollups preferred, but others are ofc fine as well")
     content = "**Thank you for support! <3**\n" \
               "I hope my bot has been useful for you, it has been a fun experience building this!\n" \
