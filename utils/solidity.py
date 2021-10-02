fe41bd0f419650bdff7bf02d368ef1139a74570e
InvisibleSymbol <pettypalkovicsreal@gmail.com>
2021-10-02 14:25:07 +0200

    clean up a bit

--- utils/solidity.py
+++ utils/solidity.py
@@ -5,3 +5,11 @@ hours = 60 * minutes
 days = 24 * hours
 weeks = 7 * days
 years = 365 * days
+
+
+def to_float(n, decimals=18):
+  return n / 10 ** decimals
+
+
+def to_int(n, decimals=18):
+  return n // 10 ** decimals
