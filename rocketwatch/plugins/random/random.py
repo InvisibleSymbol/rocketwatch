24ea9728e3a2e896edf159e81c382dcc8389db2a
InvisibleSymbol <pettypalkovicsreal@gmail.com>
2021-11-07 19:57:55 +0100

    add utc to command

--- rocketwatch/plugins/random/random.py
+++ rocketwatch/plugins/random/random.py
@@ -17,12 +17,16 @@ class Random(commands.Cog):
         """Timezones too confusing to you? Well worry no more, this command is here to help!"""
         color = Color.from_rgb(235, 142, 85)
         embed = Embed(color=color)
+        time_format = "%A %H:%M:%S %Z"
+
+        dev_time = datetime.now(tz=pytz.timezone("UTC"))
+        embed.add_field(name="Coordinated Universal Time", value=dev_time.strftime(time_format), inline=False)
 
         dev_time = datetime.now(tz=pytz.timezone("Australia/Lindeman"))
-        embed.add_field(name="Time for most of the Dev Team", value=dev_time.strftime("%A %H:%M"), inline=False)
+        embed.add_field(name="Time for most of the Dev Team", value=dev_time.strftime(time_format), inline=False)
 
         joe_time = datetime.now(tz=pytz.timezone("America/New_York"))
-        embed.add_field(name="Joe's Time", value=joe_time.strftime("%A %H:%M"), inline=False)
+        embed.add_field(name="Joe's Time", value=joe_time.strftime(time_format), inline=False)
 
         await ctx.send(embed=embed)
 
