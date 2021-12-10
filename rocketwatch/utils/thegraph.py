29417f03fbf79be5164e9db6c739f7a190ebe826
InvisibleSymbol <pettypalkovicsreal@gmail.com>
2021-12-10 22:57:29 +0100

    minor: consider all minipools for minipool distribution

--- rocketwatch/utils/thegraph.py
+++ rocketwatch/utils/thegraph.py
@@ -36,43 +36,66 @@ def get_average_commission():
 def get_minipool_count_per_node_histogram():
     query = """
 {{
-    nodes(first: {count}, skip: {offset}, orderBy: stakingMinipools, orderDirection: desc) {{
-          id
-          stakingMinipools
+    nodes(first: {count}, skip: {offset}, orderBy: id, orderDirection: desc) {{
+        minipools(first: {count}, skip: {mp_offset}, orderBy: id) {{
+            id
+        }}
+        id
     }}
 }}
     """
     # do partial request, 1000 nodes per page
-    offset = 0
+    node_offset = 0
+    minipool_offset = 0
     count = 1000
-    all_nodes = []
+    all_nodes = {}
+    # goal: we want to get the total minipool count per node
+    # get the first batch
+    # if there is a node with 1000 minipools, we do another request with an increased minipool offset, but the same node offset
+    # if there is no node with 1000 minipools, but we have 1000 nodes, we do another request with an increased node offset, but a minimum minipool offset of 0
+    # if there are less than 1000 nodes, we process them and then stop
     while True:
+        log.debug(f"Requesting nodes with offset {node_offset} and minipools with offset {minipool_offset}")
         # do the request
-        log.debug(f"Requesting {count} nodes from offset {offset}")
         response = requests.post(
             cfg["graph_endpoint"],
-            json={'query': query.format(count=count, offset=offset)}
+            json={'query': query.format(count=count, offset=node_offset, mp_offset=minipool_offset)}
         )
         # parse the response
         if "errors" in response.json():
             raise Exception(response.json()["errors"])
-        data = response.json()["data"]
-        nodes = data["nodes"]
-        if not nodes:
+        # get the data
+        data = response.json()["data"]["nodes"]
+        # process the data
+        for node in data:
+            # get the minipools
+            mp_count = len(node["minipools"])
+            # add the minipools to the node
+            if node["id"] in all_nodes:
+                all_nodes[node["id"]] += mp_count
+            else:
+                all_nodes[node["id"]] = mp_count
+        # if there was a node with 1000 minipools, we do another request with an increased minipool offset, but the same node offset
+        if max(len(node["minipools"]) for node in data) == count:
+            minipool_offset += count
+        elif len(data) == count:
+            node_offset += count
+            minipool_offset = 0
+        else:
             break
-        all_nodes.extend(nodes)
-        if len(nodes) < count:
-            break
-        offset += 1000
 
-    # create histogram of minipool count
+    # create a histogram by minipool count
     histogram = {}
-    for node in all_nodes:
-        count = node["stakingMinipools"]
-        if count not in histogram:
-            histogram[count] = 0
-        histogram[count] += 1
-    return histogram
+    for node_id, mp_count in all_nodes.items():
+        if mp_count in histogram:
+            histogram[mp_count] += 1
+        else:
+            histogram[mp_count] = 1
+
+    return [
+        (mp_count, histogram[mp_count])
+        for mp_count in sorted(histogram, reverse=True)
+    ]
 
 
 def get_reth_ratio_past_week():
