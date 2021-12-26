19e0ee4a69dc03e17d5eb4ef0460317e2c9777cf
InvisibleSymbol <34715248+InvisibleSymbol@users.noreply.github.com>
2021-12-26 02:02:13 +0100

    major: client distribution and comment word cloud commands

--- rocketwatch/plugins/proposals/proposals.py
+++ rocketwatch/plugins/proposals/proposals.py
@@ -1,20 +1,41 @@
+import html
+import random
+
 import asyncio
 import logging
 from io import BytesIO
 
 import aiohttp
+import matplotlib
 from discord import Embed, Color, File
 from discord.commands import slash_command
 from discord.ext import commands
 from matplotlib import pyplot as plt
 from motor.motor_asyncio import AsyncIOMotorClient
+from wordcloud import WordCloud
 
 from utils.cfg import cfg
+from utils.rocketpool import rp
 from utils.slash_permissions import guilds, owner_only_slash
 from utils.visibility import is_hidden
 
 log = logging.getLogger("proposals")
 log.setLevel(cfg["log_level"])
+
+CLIENTS = {
+    "N": "Nimbus",
+    "P": "Prysm",
+    "L": "Lighthouse",
+    "T": "Teku"
+}
+
+COLORS = {
+    "Nimbus": "#cc9133",
+    "Prysm": "#40bfbf",
+    "Lighthouse": "#9933cc",
+    "Teku": "#3357cc",
+    "unknown": "#B0B0B0",
+}
 
 
 class Proposals(commands.Cog):
@@ -25,14 +46,12 @@ class Proposals(commands.Cog):
         self.validator_url = "https://beaconcha.in/api/v1/validator/"
         # connect to local mongodb
         self.db = AsyncIOMotorClient("mongodb://mongodb:27017").get_database("rocketwatch")
-        self.collection = self.db.proposals
-        self.tmp_data = {}
 
     @owner_only_slash()
     async def drop_proposals(self, ctx):
         await ctx.defer()
         log.info("dropping all proposals")
-        await self.collection.delete_many({})
+        await self.db.proposals.delete_many({})
         log.info("finished dropping all proposals")
         await ctx.respond("dropped all proposals")
 
@@ -59,7 +78,7 @@ class Proposals(commands.Cog):
                 slot = int(entry[1].split(">")[-2].split("<")[0].replace(",", ""))
 
                 # break if the slot is already in the database
-                if await self.collection.count_documents({"slot": slot}) > 0:
+                if await self.db.proposals.count_documents({"slot": slot}) > 0:
                     log.debug(f"slot {slot} already in the database")
                     should_continue = False
                     continue
@@ -74,8 +93,9 @@ class Proposals(commands.Cog):
                         "client"   : parts[0].split("-")[1], "version": parts[1]
                     }
                     if len(parts) > 2:
-                        data["comment"] = " ".join(parts[2:]).lstrip("(").rstrip(")")
-                    await self.collection.replace_one({"slot": data["slot"]}, data, upsert=True)
+                        comment = " ".join(parts[2:]).lstrip("(").rstrip(")")
+                        data["comment"] = html.unescape(comment)
+                    await self.db.proposals.replace_one({"slot": data["slot"]}, data, upsert=True)
 
             if len(proposals) != 100 or not should_continue:
                 log.debug(f"stopping proposal gathering: {len(proposals)=}, {should_continue=}")
@@ -85,13 +105,34 @@ class Proposals(commands.Cog):
             await asyncio.sleep(5)
         log.info("finished gathering new proposals")
 
-    async def gather_pubkeys(self):
-        log.info("getting pubkeys for new validators...")
-        validators = await self.collection.distinct("validator", {"pubkey": {"$exists": False}})
-        # iterate in batches of 100
+    async def lookup_validators(self):
+        log.info("looking up new validators")
+        # get all validators that arent in the node_operators collection
+        validators = await self.db.proposals.aggregate([
+            {
+                '$group': {
+                    '_id': '$validator'
+                }
+            }, {
+                '$lookup': {
+                    'from': 'node_operators',
+                    'localField': '_id',
+                    'foreignField': 'validator',
+                    'as': 'data'
+                }
+            }, {
+                '$match': {
+                    'data': {
+                        '$size': 0
+                    }
+                }
+            }
+        ]).to_list(length=None)
+        validators = [str(x["_id"]) for x in validators]
+        # filter out validators that are already in the
         for i in range(0, len(validators), 100):
             log.debug(f"requesting pubkeys {i} to {i + 100}")
-            validator_ids = [str(x) for x in validators[i:i + 100]]
+            validator_ids = validators[i:i + 100]
             async with aiohttp.ClientSession() as session:
                 res = await session.get(self.validator_url + ",".join(validator_ids))
                 res = await res.json()
@@ -100,24 +141,112 @@ class Proposals(commands.Cog):
             if not isinstance(data, list):
                 data = [data]
             for validator_id, validator_data in zip(validator_ids, data):
-                await self.collection.update_many({"validator": int(validator_id)},
-                                                  {"$set": {"pubkey": validator_data["pubkey"]}})
+                # look up pubkey in rp
+                pubkey = validator_data["pubkey"]
+                # get minipool address
+                minipool = rp.call("rocketMinipoolManager.getMinipoolByPubkey", pubkey)
+                # get node operator
+                node_operator = rp.call("rocketMinipool.getNodeAddress", address=minipool)
+                # store (validator, pubkey, node_operator) in node_operators collection
+                await self.db.node_operators.replace_one({"validator": int(validator_id)},
+                                                         {"validator"    : int(validator_id),
+                                                          "pubkey"       : pubkey,
+                                                          "node_operator": node_operator},
+                                                         upsert=True)
             await asyncio.sleep(5)
-        log.info("finished gathering pubkeys")
+        log.info("finished looking up new validators")
+
+    async def chore(self, ctx):
+        msg = await ctx.respond("gathering new proposals...", ephemeral=is_hidden(ctx))
+        await self.gather_new_proposals()
+        await msg.edit(content="looking up new validators...")
+        await self.lookup_validators()
+        return msg
+
+    async def gather_latest_proposal_per_node_operator(self):
+        data = await self.db.node_operators.aggregate([
+            # get the proposals per validator
+            {
+                '$lookup': {
+                    'from': 'proposals',
+                    'localField': 'validator',
+                    'foreignField': 'validator',
+                    'as': 'proposals',
+                    'pipeline': [
+                        {
+                            '$sort': {
+                                'slot': -1
+                            }
+                        }
+                    ]
+                }
+                # only keep the latest one
+            }, {
+                '$project': {
+                    'node_operator': 1,
+                    'validator': 1,
+                    'proposal': {
+                        '$arrayElemAt': [
+                            '$proposals', 0
+                        ]
+                    }
+                }
+                # extract slot from proposal
+            }, {
+                '$project': {
+                    'node_operator': 1,
+                    'validator': 1,
+                    'slot': '$proposal.slot'
+                }
+                # group by node_operator, keep the latest slot
+            }, {
+                '$group': {
+                    '_id': '$node_operator',
+                    'slot': {
+                        '$max': '$slot'
+                    }
+                }
+                # get the proposals per node_operator using the latest slot
+            }, {
+                '$lookup': {
+                    'from': 'proposals',
+                    'localField': 'slot',
+                    'foreignField': 'slot',
+                    'as': 'proposal'
+                }
+                # only keep the latest proposal
+            }, {
+                '$project': {
+                    'node_operator': 1,
+                    'proposal': {
+                        '$arrayElemAt': [
+                            '$proposal', 0
+                        ]
+                    }
+                }
+                # extract the proposal metadata
+            }, {
+                '$project': {
+                    'node_operator': 1,
+                    'slot': '$proposal.slot',
+                    'client': '$proposal.client',
+                    'version': '$proposal.version',
+                    'comment': '$proposal.comment'
+                }
+            }
+        ]).to_list(length=None)
+        return data
 
     @slash_command(guild_ids=guilds)
     async def version_chart(self, ctx):
         await ctx.defer(ephemeral=is_hidden(ctx))
-        msg = await ctx.respond("gathering new proposals...", ephemeral=is_hidden(ctx))
-        await self.gather_new_proposals()
-        await msg.edit(content="gathering pubkeys...")
-        await self.gather_pubkeys()
+        msg = await self.chore(ctx)
         await msg.edit(content="generating version chart...")
 
         e = Embed(title="Version Chart", color=self.color)
 
         # get proposals
-        proposals = await self.collection.find().sort("slot", 1).to_list(None)
+        proposals = await self.db.proposals.find().sort("slot", 1).to_list(None)
         batch_size = int(60 / 12 * 60 * 24 * 2)
         data = {}
         versions = []
@@ -170,7 +299,126 @@ class Proposals(commands.Cog):
 
         # send data
         await msg.edit(content="", embed=e, file=File(img, filename="chart.png"))
-        # await ctx.respond(embed=e, file=File(img, "chart.png"))
+        img.close()
+
+    @slash_command(guild_ids=guilds)
+    async def client_distribution(self, ctx):
+        await ctx.defer(ephemeral=is_hidden(ctx))
+        msg = await self.chore(ctx)
+        await msg.edit(content="generating client distribution graph...")
+
+        e = Embed(title="Client Distribution", color=self.color)
+
+        # get proposals
+        proposals = await self.db.proposals.find().sort("slot", 1).to_list(None)
+
+        # get the total client distribution
+        total_data = {}
+        for proposal in proposals:
+            client = CLIENTS.get(proposal["client"], "unknown")
+            if client not in total_data:
+                total_data[client] = 0
+            total_data[client] += 1
+
+        # sort data
+        total_data = sorted(total_data.items(), key=lambda x: x[1])
+
+        # get node operators
+        node_operators = await self.gather_latest_proposal_per_node_operator()
+
+        # get total node operator count from rp
+        unknown_count = rp.call("rocketNodeManager.getNodeCount") - len(node_operators)
+
+        # get the node operator client distribution
+        node_operator_data = {}
+        for proposal in node_operators:
+            client = CLIENTS.get(proposal["client"], "unknown")
+            if client not in node_operator_data:
+                node_operator_data[client] = 0
+            node_operator_data[client] += 1
+
+        # sort data
+        node_operator_data = sorted(node_operator_data.items(), key=lambda x: x[1])
+        node_operator_data.insert(0, ("unknown", unknown_count))
+
+        # create description
+        descriptions = [
+            f"{d[0]}: {d[1]}" for d in reversed(total_data)
+        ]
+
+        descriptions = "```\n" + "\n".join(descriptions) + "```"
+        e.description = descriptions
+
+        # create 2 subplots
+        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6))
+
+        ax1.pie(
+            [x[1] for x in total_data],
+            colors=[COLORS[x[0]] for x in total_data],
+            autopct="%1.1f%%",
+            startangle=90
+        )
+        # legend
+        ax1.legend(
+            [f"{x[0]} ({x[1]})" for x in total_data],
+            loc="upper left",
+        )
+        ax1.set_title("Client Distribution based on Proposals")
+
+        ax2.pie(
+            [x[1] for x in node_operator_data],
+            colors=[COLORS[x[0]] for x in node_operator_data],
+            autopct="%1.1f%%",
+            startangle=90
+        )
+        # legend
+        ax2.legend(
+            [f"{x[0]} ({x[1]})" for x in node_operator_data],
+            loc="upper left"
+        )
+        ax2.set_title("Client Distribution based on Node Operators")
+
+        # respond with image
+        img = BytesIO()
+        plt.savefig(img, format="png")
+        img.seek(0)
+        plt.close()
+        e.set_image(url="attachment://chart.png")
+
+        # send data
+        await msg.edit(content="", embed=e, file=File(img, filename="chart.png"))
+        img.close()
+
+    @slash_command(guild_ids=guilds)
+    async def comments(self, ctx):
+        await ctx.defer(ephemeral=is_hidden(ctx))
+        msg = await self.chore(ctx)
+        await msg.edit(content="generating comments word cloud...")
+
+        # aggregate comments with their count
+        comments = await self.db.proposals.aggregate([
+            {"$match": {"comment": {"$exists": 1}}},
+            {"$group": {"_id": "$comment", "count": {"$sum": 1}}},
+            {"$sort": {"count": -1}}
+        ]).to_list(None)
+        comment_words = {x['_id']: x["count"] for x in comments}
+        wordcloud = WordCloud(width=800,
+                              height=400,
+                              margin=10,
+                              background_color="white",
+                              prefer_horizontal=0.9,
+                              # color func for random color
+                              color_func=lambda *args, **kwargs: list(COLORS.values())[random.randint(0, len(COLORS) - 2)]
+                              ).fit_words(comment_words)
+
+        # respond with image
+        img = BytesIO()
+        wordcloud.to_image().save(img, format="png")
+        img.seek(0)
+        plt.close()
+        e = Embed(title="Rocket Pool Proposal Comments", color=self.color)
+        e.set_image(url="attachment://image.png")
+        await msg.edit(content="", embed=e, file=File(img, filename="image.png"))
         img.close()
 
 
