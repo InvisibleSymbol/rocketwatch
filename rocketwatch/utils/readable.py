4fd5f7fbdebc98b0f55713cb1dd0b5886710937b
InvisibleSymbol <34715248+InvisibleSymbol@users.noreply.github.com>
2021-12-30 15:49:46 +0100

    major: add ethtx.info link to transaction hashes

--- rocketwatch/utils/readable.py
+++ rocketwatch/utils/readable.py
@@ -36,13 +36,17 @@ def uptime(time):
     return " ".join(parts[:2])
 
 
-def hex(string):
+def s_hex(string):
     return string[:10]
 
 
 def beaconchain_url(target, name=None):
     if not name:
-        name = hex(target)
+        name = s_hex(target)
     chain = cfg["rocketpool.chain"]
     prefix = "prater." if chain == "goerli" else ""
     return f"[{name}](https://{prefix}beaconcha.in/validator/{target})"
+
+
+def advanced_tnx_url(tx_hash):
+    return f"[\[A\]](https://ethtx.info/mainnet/{tx_hash})"
