137c78304ecffc4fab27feef614e9ff18c9a71d4
InvisibleSymbol <34715248+InvisibleSymbol@users.noreply.github.com>
2022-01-05 14:08:28 +0100

    chore: update pycord

--- rocketwatch/utils/slash_permissions.py
+++ rocketwatch/utils/slash_permissions.py
@@ -11,7 +11,7 @@ guilds = cfg["discord.guilds"]
 def owner_only_slash():
     return bot.slash_command(default_permission=False,
                              guild_ids=[cfg["discord.owner.server_id"]],
-                             permissions=[permissions.Permission(id=cfg["discord.owner.user_id"],
-                                                                 type=2,
-                                                                 permission=True,
-                                                                 guild_id=cfg["discord.owner.server_id"])])
+                             permissions=[permissions.CommandPermission(id=cfg["discord.owner.user_id"],
+                                                                        type=2,
+                                                                        permission=True,
+                                                                        guild_id=cfg["discord.owner.server_id"])])
