86b21b3fe7da953df28b1eadff4e92831246a9b1
InvisibleSymbol <34715248+InvisibleSymbol@users.noreply.github.com>
2022-07-29 01:43:08 +0200

    fix: add forgotten wrapper

--- rocketwatch/utils/visibility.py
+++ rocketwatch/utils/visibility.py
@@ -3,3 +3,7 @@ from discord.ext.commands import Context
 
 def is_hidden(ctx: Context):
     return ctx.channel.name not in ["random", "rocket-watch"]
+
+
+def is_hidden_weak(ctx: Context):
+    return ctx.channel.name not in ["random", "rocket-watch", "trading"]
