e40e10541bce0cbb7efbd5bb042f788e38ebbd0c
InvisibleSymbol <34715248+InvisibleSymbol@users.noreply.github.com>
2022-08-10 22:10:52 +0200

    minor: add goerli merge ttd command

--- rocketwatch/plugins/random/random.py
+++ rocketwatch/plugins/random/random.py
@@ -1,6 +1,7 @@
 import logging
 from datetime import datetime
 
+import aiohttp
 import pytz
 from discord.ext import commands
 from discord.ext.commands import Context
@@ -9,8 +10,8 @@ from discord.ext.commands import hybrid_command
 from utils.cfg import cfg
 from utils.embeds import Embed, ens, el_explorer_url
 from utils.sea_creatures import sea_creatures, get_sea_creature_for_address
-from utils.shared_w3 import w3
-from utils.visibility import is_hidden
+from utils.shared_w3 import w3, goerli_w3
+from utils.visibility import is_hidden, is_hidden_weak
 
 log = logging.getLogger("random")
 log.setLevel(cfg["log_level"])
@@ -78,6 +79,41 @@ class Random(commands.Cog):
             for holding_value, sea_creature in sea_creatures.items():
                 e.add_field(name=f"{sea_creature}:", value=f"holds over {holding_value} ETH worth of assets",
                             inline=False)
+        await ctx.send(embed=e)
+        return
+
+    @hybrid_command()
+    async def merge_ttd(self, ctx: Context):
+        """Show current merge TTD."""
+        await ctx.defer(ephemeral=is_hidden_weak(ctx))
+        e = Embed()
+        e.title = "Görli Merge"
+        try:
+            e.set_image(url=f"https://bordel.wtf/chart.png#cache_burst={int(datetime.now().timestamp())}")
+            async with aiohttp.ClientSession() as session:
+                async with session.get("https://bordel.wtf/") as resp:
+                    text = await resp.text()
+                    around = text.split("around ")[1].split(" UTC")[0]
+                    f = "%a %b %d %H:%M:%S %Y"
+                    around = int(datetime.strptime(around, f).timestamp())
+
+                    start_estimate = text.split("between ")[1].split(" and")[0]
+                    start_estimate = int(datetime.strptime(start_estimate, f).timestamp())
+
+                    end_estimate = text.split("and ")[1].split(" UTC")[0]
+                    end_estimate = int(datetime.strptime(end_estimate, f).timestamp())
+                    e.description = f"Estimate about <t:{around}:F> (<t:{around}:R>)\nbetween <t:{start_estimate}:t> & <t:{end_estimate}:t>"
+
+                    # get the latest td using w3
+                    td = goerli_w3.eth.get_block("latest").totalDifficulty
+                    e.add_field(name="Current Difficulty", value=f"{td:,}")
+
+                    ttd = text.split("Difficulty of ")[1].split(" is expected")[0]
+                    ttd = int(ttd)
+                    e.add_field(name="Target Difficulty", value=f"{ttd:,}")
+        except Exception as e:
+            log.error(e)
+            e.description = "Merge prob already happened"
         await ctx.send(embed=e)
         return
 
