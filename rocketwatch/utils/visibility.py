1340241d7a4dfbc16a07a37d185202bfb9e7ce7b
InvisibleSymbol <34715248+InvisibleSymbol@users.noreply.github.com>
2022-08-22 09:02:13 +0200

    Fix visibility check after channel rename

--- rocketwatch/utils/visibility.py
+++ rocketwatch/utils/visibility.py
@@ -2,8 +2,8 @@ from discord.ext.commands import Context
 
 
 def is_hidden(ctx: Context):
-    return ctx.channel.name not in ["random", "rocket-watch"]
+    return any(w in ctx.channel.name for w in ["random", "rocket-watch"])
 
 
 def is_hidden_weak(ctx: Context):
-    return ctx.channel.name not in ["random", "rocket-watch", "trading"]
+    return any(w in ctx.channel.name for w in ["random", "rocket-watch", "trading"])
