08b6c83804b83a70f5bd359ccaf38a6e4f2a3831
InvisibleSymbol <34715248+InvisibleSymbol@users.noreply.github.com>
2022-11-22 19:16:03 +0100

    ser

--- rocketwatch/plugins/dai/dai.py
+++ rocketwatch/plugins/dai/dai.py
@@ -105,10 +105,10 @@ class DAI(commands.Cog):
         e.description = "**5 Largest Vaults:**\n"
         for v in vaults:
             e.description += f"{el_explorer_url(w3.toChecksumAddress(v['owner']))}:\n" \
-                             f"<:VOID:721787344138797116>Borrowed `{int(v['principal']):,}` DAI" \
-                             f" with `{v['collateral']:.2f}` rETH as collateral (`{int(v['collateralization'])}%`)\n" \
-                             f"<:VOID:721787344138797116>Liquidation Price: `{int(v['liquidation_price'])}` rETH/DAI" \
-                             f" Accrued Fees: `{v['accrued_fees']:.2f} DAI`"
+                             f"<:VOID:721787344138797116>Borrowed `{int(v['principal']):,} DAI`" \
+                             f" with `{v['collateral']:.2f} rETH` as collateral (`{int(v['collateralization'])}%`)\n" \
+                             f"<:VOID:721787344138797116>Liquidation Price: `{int(v['liquidation_price'])} rETH/DAI`" \
+                             f" Accrued Fees: `{v['accrued_fees']:.2f} DAI`\n"
         await ctx.send(embed=e)
 
 
