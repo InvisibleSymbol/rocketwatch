f6cfef01a9ccadae1d505ed99742be014a5be165
InvisibleSymbol <34715248+InvisibleSymbol@users.noreply.github.com>
2023-04-18 19:40:45 +0200

    update dp command to be more accurate

--- rocketwatch/plugins/deposit_pool/deposit_pool.py
+++ rocketwatch/plugins/deposit_pool/deposit_pool.py
@@ -28,37 +28,28 @@ async def get_dp():
     deposit_cap = solidity.to_int(rp.call("rocketDAOProtocolSettingsDeposit.getMaximumDepositPoolSize"))
     e.add_field(name="Maximum Size:", value=f"{humanize.intcomma(deposit_cap)} ETH")
 
-    current_node_demand = solidity.to_float(rp.call("rocketNetworkFees.getNodeDemand"))
-
     if deposit_cap - deposit_pool < 0.01:
         e.add_field(name="Status:", value="Deposit Pool Cap Reached!", inline=False)
     else:
         percentage_filled = round(deposit_pool / deposit_cap * 100, 2)
-        free_capacity = deposit_cap - deposit_pool
-        if current_node_demand <= 0:
-            free_capacity += current_node_demand * -1
+        free_capacity = solidity.to_float(rp.call("rocketDepositPool.getMaximumDepositAmount"))
         free_capacity = round(free_capacity, 3)
         e.add_field(name="Status:",
                     value=f"Buffer {percentage_filled}% Full. Enough space for {humanize.intcomma(free_capacity)} more ETH",
                     inline=False)
 
-    current_commission = solidity.to_float(rp.call("rocketNetworkFees.getNodeFee")) * 100
-    e.add_field(name="Commission Rate:", value=f"{round(current_commission, 2)}%")
-
     queue_length = rp.call("rocketMinipoolQueue.getTotalLength")
-    e.add_field(name="Current Queue:", value=f"{humanize.intcomma(queue_length)} Minipools")
-    e.add_field(name="Enough For:",
-                value=f"**`{deposit_pool // 16:>4.0f}`** 16 ETH Minipools (16 ETH from DP)" +
-                      (f"\n**`{deposit_pool // 24:>4.0f}`** 8 ETH Minipools (24 ETH from DP)" if deposit_pool > 24 else "") +
-                      (f"\n**`{deposit_pool // 32:>4.0f}`** Credit Minipools (32 ETH from DP)" if deposit_pool > 32 else ""),
-                inline=False)
+    if queue_length > 0:
+        e.add_field(name="Current Queue:", value=f"{humanize.intcomma(queue_length)} Minipools", inline=False)
 
-    img = BytesIO()
-    if rendered_graph := get_graph(img, current_commission, current_node_demand):
-        e.set_image(url="attachment://graph.png")
-        return e, img
-    else:
-        return e, None
+    if deposit_pool // 16 > 0:
+        e.add_field(name="Enough For:",
+                    value=f"**`{deposit_pool // 16:>4.0f}`** 16 ETH Minipools (16 ETH from DP)" +
+                          f"\n**`{deposit_pool // 24:>4.0f}`** 8 ETH Minipools (24 ETH from DP)" +
+                          f"\n**`{deposit_pool // 32:>4.0f}`** Credit Minipools (32 ETH from DP)",
+                    inline=False)
+
+    return e, None
 
 
 class DepositPool(commands.Cog):
