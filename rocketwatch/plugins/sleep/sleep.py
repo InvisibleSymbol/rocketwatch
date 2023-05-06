5a59e4efa738c049d7210ad32635751181f57d7b
InvisibleSymbol <34715248+InvisibleSymbol@users.noreply.github.com>
2023-05-07 00:59:19 +0200

    sleep: 60 day range, show nighttime, other adjustments

--- rocketwatch/plugins/sleep/sleep.py
+++ rocketwatch/plugins/sleep/sleep.py
@@ -11,6 +11,7 @@ from discord.ext.commands import Context, hybrid_command
 from utils.cfg import cfg
 from utils.embeds import Embed
 from utils.visibility import is_hidden
+from suntimes import SunTimes
 
 log = logging.getLogger("sleep")
 log.setLevel(cfg["log_level"])
@@ -19,13 +20,14 @@ log.setLevel(cfg["log_level"])
 class Oura(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.sun = SunTimes(latitude=48.210033, longitude=16.363449)
 
     @hybrid_command()
     async def sleep_schedule(self, ctx: Context):
         await ctx.defer(ephemeral=is_hidden(ctx))
         e = Embed(title="Invis's Sleep Schedule")
         current_date = datetime.datetime.now()
-        start_date = current_date - datetime.timedelta(days=30)
+        start_date = current_date - datetime.timedelta(days=60)
         end_date = current_date
         res = requests.get("https://api.ouraring.com/v2/usercollection/sleep",
                            params={"start_date": start_date.strftime("%Y-%m-%d"),
@@ -87,22 +89,35 @@ class Oura(commands.Cog):
         day_of_week_colors = ["#ff0000", "#ff8000", "#ffff00", "#80ff00", "#00ff00", "#00ff80", "#00ffff"]
         # plot
         fig, ax = plt.subplots()
-        # create horizontal dark gray line at 0am across the entire graph
+        # create horizontal dark gray line at midnight and noon
         ax.axhline(y=18, color="#808080", linewidth=1)
+        ax.axhline(y=18-12, color="#808080", linewidth=1)
         for i, (day, sleeps) in enumerate(daily_sleep.items()):
             # plot each sleep, from top to bottom
+            sunset_prev = self.sun.setlocal(datetime.datetime.fromisoformat(day) - datetime.timedelta(days=1))
+            sunrise_curr = self.sun.riselocal(datetime.datetime.fromisoformat(day))
+            sunset_curr = self.sun.setlocal(datetime.datetime.fromisoformat(day))
+            bottom_of_chart = datetime.datetime(year=sunrise_curr.year, month=sunrise_curr.month, day=sunrise_curr.day, hour=18,
+                                                tzinfo=sunrise_curr.tzinfo)
+            ax.bar(i, (sunrise_curr - sunset_prev).total_seconds() / 3600,
+                   bottom=(bottom_of_chart - sunrise_curr).total_seconds() / 3600, color="#AAAAAA", width=1, alpha=0.5)
+            if sunset_curr.hour < 18:
+                ax.bar(i, (bottom_of_chart - sunset_curr).total_seconds() / 3600,
+                       bottom=0, color="#AAAAAA", width=1, alpha=0.5)
             for sleep in sleeps:
                 color = day_of_week_colors[sleep["weekday"]]
                 ax.bar(i, sleep["duration"].total_seconds() / 3600, bottom=((24 * 60 * 60) - sleep[
-                    "relative_start"].total_seconds() - sleep["duration"].total_seconds()) / 3600, color=color)
+                    "relative_start"].total_seconds() - sleep["duration"].total_seconds()) / 3600, color=color, alpha=0.8)
         # set x axis labels, only every 7th day
-        ax.set_xticks(range(len(daily_sleep), 0, -7))
-        ax.set_xticklabels([day for i, (day, _) in enumerate(reversed(daily_sleep.items())) if i % 7 == 0])
+        ax.set_xticks(range(len(daily_sleep) - 1, 0, -14))
+        ax.set_xticklabels([day for i, (day, _) in enumerate(reversed(daily_sleep.items())) if i % 14 == 0])
         # set y axis labels
         ax.set_yticks(range(0, 25, 2))
         ax.set_yticklabels([f"{i}:00" if i >= 0 else f"{24 + i}:00" for i in range(18, -7, -2)])
         # set y limit
         ax.set_ylim(0, 24)
+        # set x limit
+        ax.set_xlim(-1, len(daily_sleep))
         # grid
         ax.grid(True)
         ax.set_axisbelow(True)
