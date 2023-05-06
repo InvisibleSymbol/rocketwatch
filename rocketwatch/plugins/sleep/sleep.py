1cc88c1b74799d60db07ab6540b3939742f59722
InvisibleSymbol <34715248+InvisibleSymbol@users.noreply.github.com>
2023-05-06 23:58:18 +0200

    sleep: use 18-18 range instead

--- rocketwatch/plugins/sleep/sleep.py
+++ rocketwatch/plugins/sleep/sleep.py
@@ -53,15 +53,15 @@ class Oura(commands.Cog):
                 continue
             start_date = datetime.datetime.fromisoformat(sleep["bedtime_start"]) + datetime.timedelta(seconds=sleep["latency"])
             # the start day is the next day if we are past 12pm, otherwise it is the current day
-            start_day = start_date + datetime.timedelta(days=1) if start_date.hour >= 12 else start_date
+            start_day = start_date + datetime.timedelta(days=1) if start_date.hour >= 18 else start_date
             # format to string
             start_day = start_day.strftime("%Y-%m-%d")
             end_date = start_date + datetime.timedelta(seconds=sleep["total_sleep_duration"])
             # the end day is the next day if we are past 12pm, otherwise it is the current day
-            end_day = end_date + datetime.timedelta(days=1) if end_date.hour >= 12 else end_date
+            end_day = end_date + datetime.timedelta(days=1) if end_date.hour >= 18 else end_date
             # format to string
             end_day = end_day.strftime("%Y-%m-%d")
-            thresh = datetime.datetime(year=end_date.year, month=end_date.month, day=end_date.day, hour=12,
+            thresh = datetime.datetime(year=end_date.year, month=end_date.month, day=end_date.day, hour=18,
                                        tzinfo=end_date.tzinfo)
             if start_day not in daily_sleep:
                 daily_sleep[start_day] = []
@@ -87,6 +87,8 @@ class Oura(commands.Cog):
         day_of_week_colors = ["#ff0000", "#ff8000", "#ffff00", "#80ff00", "#00ff00", "#00ff80", "#00ffff"]
         # plot
         fig, ax = plt.subplots()
+        # create horizontal dark gray line at 0am across the entire graph
+        ax.axhline(y=18, color="#808080", linewidth=1)
         for i, (day, sleeps) in enumerate(daily_sleep.items()):
             # plot each sleep, from top to bottom
             for sleep in sleeps:
@@ -94,15 +96,16 @@ class Oura(commands.Cog):
                 ax.bar(i, sleep["duration"].total_seconds() / 3600, bottom=((24 * 60 * 60) - sleep[
                     "relative_start"].total_seconds() - sleep["duration"].total_seconds()) / 3600, color=color)
         # set x axis labels, only every 7th day
-        ax.set_xticks(range(0, len(daily_sleep), 7))
-        ax.set_xticklabels([day for i, (day, _) in enumerate(daily_sleep.items()) if i % 7 == 0])
+        ax.set_xticks(range(len(daily_sleep), 0, -7))
+        ax.set_xticklabels([day for i, (day, _) in enumerate(reversed(daily_sleep.items())) if i % 7 == 0])
         # set y axis labels
         ax.set_yticks(range(0, 25, 2))
-        ax.set_yticklabels([f"{i}:00" if i >= 0 else f"{24 + i}:00" for i in range(12, -13, -2)])
+        ax.set_yticklabels([f"{i}:00" if i >= 0 else f"{24 + i}:00" for i in range(18, -7, -2)])
         # set y limit
         ax.set_ylim(0, 24)
         # grid
         ax.grid(True)
+        ax.set_axisbelow(True)
         # set title
         ax.set_title("Invis's Sleep Schedule")
 
