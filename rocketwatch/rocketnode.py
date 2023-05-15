e0d28e49c2022fa9e872f096085e36f9e8b15b35
InvisibleSymbol <34715248+InvisibleSymbol@users.noreply.github.com>
2023-05-15 10:13:07 +0200

    rocketnode: why does reversed() return

--- rocketwatch/rocketnode.py
+++ rocketwatch/rocketnode.py
@@ -194,7 +194,7 @@ class Task:
             f_creations = mm.events.MinipoolCreated.createFilter(fromBlock=block_start, toBlock=block_end)
             events.extend(f_creations.get_all_entries())
             events = sorted(events, key=lambda x: (x['blockNumber'], x['transactionIndex'], x['logIndex']))
-            events = reversed(events)
+            events = list(reversed(events))
             # map to pairs of 2
             prepared_events = []
             last_addition_is_creation = False
