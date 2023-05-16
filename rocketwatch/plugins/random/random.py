376575b7784c339803e29d4fc79834acdb4c9aa2
Sourcery AI <>
2023-05-16 20:14:24 +0000

    'Refactored by Sourcery'

--- rocketwatch/plugins/random/random.py
+++ rocketwatch/plugins/random/random.py
@@ -150,16 +150,15 @@ class Random(commands.Cog):
                 e.description = "Invalid address"
                 await ctx.send(embed=e)
                 return
-            creature = get_sea_creature_for_address(address)
-            if not creature:
-                e.description = f"No sea creature for {address}"
-            else:
+            if creature := get_sea_creature_for_address(address):
                 # get the required holding from the dictionary
                 required_holding = [h for h, c in sea_creatures.items() if c == creature[0]][0]
                 e.add_field(name="Visualization", value=el_explorer_url(address, prefix=creature), inline=False)
                 e.add_field(name="Required holding for emoji", value=f"{required_holding * len(creature)} ETH", inline=False)
                 holding = get_holding_for_address(address)
                 e.add_field(name="Actual Holding", value=f"{holding:.0f} ETH", inline=False)
+            else:
+                e.description = f"No sea creature for {address}"
         else:
             e.title = "Possible Sea Creatures"
             e.description = "RPL (both old and new), rETH and ETH are consider as assets for the sea creature determination!"
