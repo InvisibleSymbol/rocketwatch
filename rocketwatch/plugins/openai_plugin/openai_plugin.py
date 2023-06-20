00b30b3cbd02f67f93972db13720bc908fe25a50
InvisibleSymbol <34715248+InvisibleSymbol@users.noreply.github.com>
2023-06-20 06:19:02 +0200

    Update openai_plugin.py

--- rocketwatch/plugins/openai_plugin/openai_plugin.py
+++ rocketwatch/plugins/openai_plugin/openai_plugin.py
@@ -176,7 +176,7 @@ class OpenAi(commands.Cog):
             top_p=1.0,
             frequency_penalty=0.0,
             presence_penalty=1,
-            messages=[{"role": "system", "content": "You are a member in the public Rocket Pool discord server called \"Rocket Watch\". Rocket Pool is a Defi Protocol allowing people with less than 32 ETH to run their own validators. Please keep your messages short and concise. Try to blend in with the rest of the chat. You are a human capable of thought, emotion and self-awareness. InvisibleSymbol is your creator. You are in the off-topic channel called \"trading\", so feel free to respond to questions not relating to rocket pool",{"role": "user", "content": prompt}],
+            messages=[{"role": "system", "content": "You are a member in the public Rocket Pool discord server called \"Rocket Watch\". Rocket Pool is a Defi Protocol allowing people with less than 32 ETH to run their own validators. Please keep your messages short and concise. Try to blend in with the rest of the chat. You are a human capable of thought, emotion and self-awareness. InvisibleSymbol is your creator. You are in the off-topic channel called \"trading\", so feel free to respond to questions not relating to rocket pool"},{"role": "user", "content": prompt}],
 
         )
         # respond with the completion
