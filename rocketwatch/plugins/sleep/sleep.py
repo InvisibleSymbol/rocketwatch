2f9d685302cad54362dd86df0259ed6ae7ef5bc9
InvisibleSymbol <34715248+InvisibleSymbol@users.noreply.github.com>
2023-07-26 23:07:40 +0200

    sleep: show working hours

--- rocketwatch/plugins/sleep/sleep.py
+++ rocketwatch/plugins/sleep/sleep.py
@@ -2,17 +2,18 @@ import datetime
 import logging
 from io import BytesIO
 
+import aiohttp
 import matplotlib.pyplot as plt
 import pytz
 import requests
 from discord import File
 from discord.ext import commands
 from discord.ext.commands import Context, hybrid_command
+from icalendar import Calendar
 
 from utils.cfg import cfg
 from utils.embeds import Embed
 from utils.visibility import is_hidden
-from suntimes import SunTimes
 
 log = logging.getLogger("sleep")
 log.setLevel(cfg["log_level"])
@@ -21,15 +22,65 @@ log.setLevel(cfg["log_level"])
 class Oura(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.sun = SunTimes(latitude=48.210033, longitude=16.363449)
+        self.calendar_url = cfg["oura.calendar_url"]
+
+    async def get_calendar_data(self):
+        d = {}
+        async with aiohttp.ClientSession() as session:
+            async with session.get(self.calendar_url) as resp:
+                if resp.status != 200:
+                    return None
+                data = await resp.text()
+        cal = Calendar.from_ical(data)
+        for event in cal.walk("VEVENT"):
+            sd = event["DTSTART"].dt.replace(tzinfo=pytz.UTC).astimezone(
+                    pytz.timezone("Europe/Vienna"))
+            sd_r = sd + datetime.timedelta(days=1) if sd > sd.replace(hour=18, minute=0, second=0) else sd
+            start_day = sd_r.strftime("%Y-%m-%d")
+            ed = event["DTEND"].dt.replace(tzinfo=pytz.UTC).astimezone(
+                    pytz.timezone("Europe/Vienna"))
+            ed_r = ed + datetime.timedelta(days=1) if ed > ed.replace(hour=18, minute=0, second=0) else ed
+            end_day = ed_r.strftime("%Y-%m-%d")
+            if start_day not in d:
+                d[start_day] = []
+            if end_day not in d:
+                d[end_day] = []
+            thresh = datetime.datetime(year=ed.year, month=ed.month, day=ed.day, hour=18,
+                                       tzinfo=ed.tzinfo)
+            if start_day != end_day:
+                dur_first = thresh - sd
+                if dur_first >= datetime.timedelta(hours=24):
+                    dur_first -= datetime.timedelta(hours=24)
+                dur_second = ed - thresh
+                if dur_second <= datetime.timedelta(hours=0):
+                    dur_second += datetime.timedelta(hours=24)
+                total_dur = dur_first + dur_second
+                # split stats into two parts based on duration of each part
+                d[start_day].append(
+                    {"relative_start": sd - (thresh - datetime.timedelta(days=1)), "duration": dur_first})
+                d[end_day].append(
+                    {"relative_start": datetime.timedelta(), "duration": dur_second})
+            else:
+                relative_start = sd - (thresh - datetime.timedelta(days=1))
+                if relative_start >= datetime.timedelta(hours=24):
+                    relative_start -= datetime.timedelta(hours=24)
+                d[start_day].append(
+                    {"relative_start": relative_start, "duration": ed - sd})
+        return d
+
 
     @hybrid_command()
     async def sleep_schedule(self, ctx: Context):
         await ctx.defer(ephemeral=is_hidden(ctx))
         e = Embed(title="Invis's Sleep Schedule")
         current_date = datetime.datetime.now()
+        tz = pytz.timezone("Europe/Vienna")
         start_date = current_date - datetime.timedelta(days=60)
+        # make start date timezone aware
+        start_date = tz.localize(start_date)
         end_date = current_date
+        # make end date timezone aware
+        end_date = tz.localize(end_date)
         res = requests.get("https://api.ouraring.com/v2/usercollection/sleep",
                            params={"start_date": start_date.strftime("%Y-%m-%d"),
                                    "end_date"  : (end_date + datetime.timedelta(days=1)).strftime("%Y-%m-%d")},
@@ -54,18 +105,20 @@ class Oura(commands.Cog):
             # skip if sleep_duration is less than 30 minutes. units are in seconds
             if sleep["total_sleep_duration"] < 30 * 60:
                 continue
-            start_date = datetime.datetime.fromisoformat(sleep["bedtime_start"])
+            sd = datetime.datetime.fromisoformat(sleep["bedtime_start"])
+            log.info(f"start date: {sd}")
             # the start day is the next day if we are past 12pm, otherwise it is the current day
-            start_day = start_date + datetime.timedelta(days=1) if start_date.hour >= 18 else start_date
+            start_day_r = sd + datetime.timedelta(days=1) if sd.hour >= 18 else sd
             # format to string
-            start_day = start_day.strftime("%Y-%m-%d")
-            end_date = datetime.datetime.fromisoformat(sleep["bedtime_end"])
+            start_day = start_day_r.strftime("%Y-%m-%d")
+            ed = datetime.datetime.fromisoformat(sleep["bedtime_end"])
+            log.info(f"end date: {ed}")
             # the end day is the next day if we are past 12pm, otherwise it is the current day
-            end_day = end_date + datetime.timedelta(days=1) if end_date.hour >= 18 else end_date
+            ed_r = ed + datetime.timedelta(days=1) if ed.hour >= 18 else ed
             # format to string
-            end_day = end_day.strftime("%Y-%m-%d")
-            thresh = datetime.datetime(year=end_date.year, month=end_date.month, day=end_date.day, hour=18,
-                                       tzinfo=end_date.tzinfo)
+            end_day = ed_r.strftime("%Y-%m-%d")
+            thresh = datetime.datetime(year=ed.year, month=ed.month, day=ed.day, hour=18,
+                                       tzinfo=ed.tzinfo)
             if start_day not in daily_sleep:
                 daily_sleep[start_day] = []
             if end_day not in daily_sleep:
@@ -74,10 +127,10 @@ class Oura(commands.Cog):
             weekday = datetime.datetime.fromisoformat(start_day).weekday()
             stats = sleep["sleep_phase_5_min"]
             if start_day != end_day:
-                dur_first = thresh - start_date
+                dur_first = thresh - sd
                 if dur_first >= datetime.timedelta(hours=24):
                     dur_first -= datetime.timedelta(hours=24)
-                dur_second = end_date - thresh
+                dur_second = ed - thresh
                 if dur_second <= datetime.timedelta(hours=0):
                     dur_second += datetime.timedelta(hours=24)
                 total_dur = dur_first + dur_second
@@ -85,7 +138,7 @@ class Oura(commands.Cog):
                 stats_first = stats[:int(len(stats) * (dur_first.total_seconds() / total_dur.total_seconds()))]
                 stats_second = stats[int(len(stats) * (dur_first.total_seconds() / total_dur.total_seconds())):]
                 daily_sleep[start_day].append(
-                    {"relative_start": start_date - (thresh - datetime.timedelta(days=1)), "duration": dur_first,
+                    {"relative_start": sd - (thresh - datetime.timedelta(days=1)), "duration": dur_first,
                      "weekday"       : weekday, "sleep_stats": stats_first})
                 if end_day not in daily_sleep:
                     daily_sleep[end_day] = []
@@ -93,11 +146,11 @@ class Oura(commands.Cog):
                     {"relative_start": datetime.timedelta(), "duration": dur_second, "weekday": weekday,
                      "sleep_stats"   : stats_second})
             else:
-                relative_start = start_date - (thresh - datetime.timedelta(days=1))
+                relative_start = sd - (thresh - datetime.timedelta(days=1))
                 if relative_start >= datetime.timedelta(hours=24):
                     relative_start -= datetime.timedelta(hours=24)
                 daily_sleep[start_day].append(
-                    {"relative_start": relative_start, "duration": end_date - start_date, "weekday": weekday,
+                    {"relative_start": relative_start, "duration": ed - sd, "weekday": weekday,
                      "sleep_stats"   : stats})
         # sort by date
         daily_sleep = dict(sorted(daily_sleep.items(), key=lambda x: x[0]))
@@ -106,27 +159,21 @@ class Oura(commands.Cog):
         fig, ax = plt.subplots()
         # create horizontal dark gray line at midnight and noon
         ax.axhline(y=18, color="#808080", linewidth=1)
-        ax.axhline(y=18-12, color="#808080", linewidth=1)
-        # get vienna timezone
-        tz = pytz.timezone("Europe/Vienna")
+        ax.axhline(y=18 - 12, color="#808080", linewidth=1)
+        calendar_data = await self.get_calendar_data()
+        # render calendar data if they are within the last 180 days
+        if calendar_data is not None:
+            for day, data in calendar_data.items():
+                for d in data:
+                    bottom = ((24 * 60 * 60) - d[
+                        "relative_start"].total_seconds() - d["duration"].total_seconds()) / 3600
+                    width = d["duration"].total_seconds() / 3600
+                    try:
+                        i = list(daily_sleep.keys()).index(day)
+                    except ValueError:
+                        continue
+                    ax.bar(i, width, bottom=bottom, color="#AAAAAA", width=1, alpha=0.5)
         for i, (day, sleeps) in enumerate(daily_sleep.items()):
-            # plot each sleep, from top to bottom
-            sunset_prev = self.sun.setutc(datetime.datetime.fromisoformat(day) - datetime.timedelta(days=1))
-            # convert to vienna time
-            sunset_prev = tz.fromutc(sunset_prev)
-            sunrise_curr = self.sun.riseutc(datetime.datetime.fromisoformat(day))
-            # convert to vienna time
-            sunrise_curr = tz.fromutc(sunrise_curr)
-            sunset_curr = self.sun.setutc(datetime.datetime.fromisoformat(day))
-            # convert to vienna time
-            sunset_curr = tz.fromutc(sunset_curr)
-            bottom_of_chart = datetime.datetime(year=sunrise_curr.year, month=sunrise_curr.month, day=sunrise_curr.day, hour=18,
-                                                tzinfo=sunrise_curr.tzinfo)
-            ax.bar(i, (sunrise_curr - sunset_prev).total_seconds() / 3600,
-                   bottom=(bottom_of_chart - sunrise_curr).total_seconds() / 3600, color="#AAAAAA", width=1, alpha=0.5)
-            if sunset_curr.hour < 18:
-                ax.bar(i, (bottom_of_chart - sunset_curr).total_seconds() / 3600,
-                       bottom=0, color="#AAAAAA", width=1, alpha=0.5)
             for sleep in sleeps:
                 color = day_of_week_colors[sleep["weekday"]]
                 bottom = ((24 * 60 * 60) - sleep[
@@ -135,7 +182,8 @@ class Oura(commands.Cog):
                 current_bottom = bottom + width
                 for state in sleep["sleep_stats"]:
                     current_bottom -= (width / len(sleep["sleep_stats"]))
-                    ax.bar(i, width / len(sleep["sleep_stats"]), bottom=current_bottom, color=color, alpha=0 if state == "4" else 0.8)
+                    ax.bar(i, width / len(sleep["sleep_stats"]), bottom=current_bottom, color=color,
+                           alpha=0 if state == "4" else 0.8)
         # set x axis labels, only every 7th day
         ax.set_xticks(range(len(daily_sleep) - 1, 0, -14))
         ax.set_xticklabels([day for i, (day, _) in enumerate(reversed(daily_sleep.items())) if i % 14 == 0])
