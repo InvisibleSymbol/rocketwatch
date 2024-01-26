eebb64d1a6ea3ef5ba09d81af930416181792741
InvisibleSymbol <34715248+InvisibleSymbol@users.noreply.github.com>
2024-01-26 23:00:15 +0100

    openai: gpt4-turbo

--- rocketwatch/plugins/openai_plugin/openai_plugin.py
+++ rocketwatch/plugins/openai_plugin/openai_plugin.py
@@ -3,14 +3,14 @@ import re
 from datetime import datetime, timedelta, timezone
 from io import BytesIO
 
-import openai
 import pytz
+import tiktoken
 from discord import File, DeletedReferencedMessage
 from discord.ext import commands
 from discord.ext.commands import Context
 from discord.ext.commands import hybrid_command
 from motor.motor_asyncio import AsyncIOMotorClient
-from transformers import GPT2TokenizerFast
+from openai import AsyncOpenAI
 
 from utils.cfg import cfg
 from utils.embeds import Embed
@@ -22,16 +22,17 @@ log.setLevel(cfg["log_level"])
 class OpenAi(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        openai.api_key = cfg["openai.secret"]
+        self.client = AsyncOpenAI(
+            # This is the default and can be omitted
+            api_key=cfg["openai.secret"],
+        )
         # log all possible engines
-        models = openai.Model.list()
-        log.debug([d.id for d in models.data])
-        self.tokenizer = GPT2TokenizerFast.from_pretrained("gpt2")
+        self.tokenizer = tiktoken.encoding_for_model("gpt-4-turbo")
         self.db = AsyncIOMotorClient(cfg["mongodb_uri"]).get_database("rocketwatch")
 
     @classmethod
     def message_to_text(cls, message, index):
-        text = f"{message.author.global_name or message.author.name} at {message.created_at.strftime('%H:%M')}:\n {message.content}" #  {{message:{index}}}
+        text = f"{message.author.global_name or message.author.name} at {message.created_at.strftime('%H:%M')} ({{message:{index}}}):\n {message.content}"
 
         # if there is an image attached, add it to the text as a note
         metadata = []
@@ -40,10 +41,12 @@ class OpenAi(commands.Cog):
         if message.embeds:
             metadata.append(f"{len(message.embeds)} embeds")
         # replies and make sure the reference is not deleted
-        if message.reference and not isinstance(message.reference.resolved, DeletedReferencedMessage) and message.reference.resolved:
+        if message.reference and not isinstance(message.reference.resolved,
+                                                DeletedReferencedMessage) and message.reference.resolved:
             # show name of referenced message author
             # and the first 10 characters of the referenced message
-            metadata.append(f"reply to \"{message.reference.resolved.content[:32]}…\" from {message.reference.resolved.author.name}")
+            metadata.append(
+                f"reply to \"{message.reference.resolved.content[:32]}…\" from {message.reference.resolved.author.name}")
         if metadata:
             text += f" <{', '.join(metadata)}>\n"
         # replace all <@[0-9]+> with the name of the user
@@ -58,22 +61,47 @@ class OpenAi(commands.Cog):
         await ctx.defer(ephemeral=True)
         last_ts = await self.db["last_summary"].find_one({"channel_id": ctx.channel.id})
         # ratelimit
-        if last_ts and (datetime.now(timezone.utc) - last_ts["timestamp"].replace(tzinfo=pytz.utc)) < timedelta(minutes=60):
-            await ctx.send("You can only summarize once every hour.", ephemeral=True)
-            return
+        if last_ts and (datetime.now(timezone.utc) - last_ts["timestamp"].replace(tzinfo=pytz.utc)) < timedelta(minutes=60 * 6):
+             await ctx.send("You can only summarize once every 6 hours.", ephemeral=True)
+             return
         if ctx.channel.id not in [405163713063288832]:
             await ctx.send("You can't summarize here.", ephemeral=True)
             return
         msg = await ctx.channel.send("Summarizing chat…")
         last_ts = last_ts["timestamp"].replace(tzinfo=pytz.utc) if last_ts and "timestamp" in last_ts else datetime.now(timezone.utc) - timedelta(days=365)
-        response, prompt, msgs = await self.prompt_model(ctx.channel, "Please summarize the above chat log using a very short chronological bullet list! The bullet list MUST be short. You MUST constrain topics to a single bullet point and you MUST skip uninteresting topics! You MUST link to a single message index related to the bullet list entry at the end of each entry with the following syntax: {message:0}, with 0 being the index of the message. You MUST not reference multiple message indexes!!" , last_ts)
+        prompt = (
+            "Task Description:\n"
+            "I need a summary of the previous chat log. This summary should be presented in the form of a bullet list.\n\n"
+            "Format and Length Requirements:\n"
+            "- The bullet list must be very short and concise.\n"
+            "- Each bullet point should chronologically represent a distinct topic discussed in the chat log.\n\n"
+            "Content Constraints:\n"
+            "- Limit each topic to a single bullet point in the list.\n"
+            "- Omit any topics that are uninteresting or not crucial to the overall understanding of the chat log.\n"
+            "- If any content in the chat log goes against guidelines, refer to it in a safe and compliant manner, without detailing the specific content.\n\n"
+            "Referencing Requirements:\n"
+            "- At the end of each bullet point, include a reference to the relevant message from the chat log.\n"
+            "- Use the following syntax for referencing: `{message:index}`, where `index` is the numerical index of the message.\n"
+            "- Ensure that each bullet point references only one message index. Multiple message indexes per bullet point are not allowed.\n\n"
+            "Response Instruction:\n"
+            "- Respond only with the bullet list summary as specified. Do not include any additional commentary or response outside of this list.\n\n"
+            "Example Output:\n"
+            "----------------\n"
+            "- User asked about the weather in Paris. {message:2}\n"
+            "- Discussed the history of the Eiffel Tower. {message:4}\n"
+            "- Advised on popular restaurants in Paris. {message:7}\n"
+            "----------------\n\n"
+            "Command to Begin:\n"
+            "Please begin the task now."
+        )
+        response, prompt, msgs = await self.prompt_model(ctx.channel, prompt, last_ts)
         if not response:
             await msg.delete()
             await ctx.send(content="Not enough messages to summarize.")
             return
         es = [Embed()]
         es[0].title = f"Chat Summarization of {msgs} messages since {last_ts.strftime('%Y-%m-%d %H:%M')}"
-        res = response["choices"][0]["message"]["content"]
+        res = response.choices[0].message.content
         # split content in multiple embeds if it is too long. limit for description is 4096
         while len(res):
             if len(res) > 4096:
@@ -91,9 +119,9 @@ class OpenAi(commands.Cog):
             else:
                 es[-1].description = res
                 res = ""
-        token_usage = response['usage']['total_tokens']
+        token_usage = response.usage.prompt_tokens + (response.usage.completion_tokens * 3) # completion tokens are 3x more expensive
         es[-1].set_footer(
-            text=f"Request cost: ${token_usage / 1000 * 0.003:.2f} | Tokens: {token_usage} | /donate if you like this command")
+            text=f"Request cost: ${token_usage / 1000 * 0.01:.2f} | Tokens: {response.usage.total_tokens} | /donate if you like this command")
         # attach the prompt as a file
         f = BytesIO(prompt.encode("utf-8"))
         f.name = "prompt._log"
@@ -114,18 +142,21 @@ class OpenAi(commands.Cog):
         messages = [message async for message in channel.history(limit=1024) if message.content != ""]
         messages = [message for message in messages if message.author.id != self.bot.user.id]
         messages = [message for message in messages if message.created_at > cut_off_ts]
-        if len(messages) < 32:
+        if len([]) < 320:
             return None, None, None
         prefix = "The following is a chat log. Everything prefixed with `>` is a quote."
-        while (l := len(self.tokenizer(self.generate_prompt(messages, prefix, prompt))['input_ids'])) > (16384 - 512):
+        log.info(f"Prompt len: {len(self.tokenizer.encode(self.generate_prompt(messages, prefix, prompt)))}")
+        while len(self.tokenizer.encode(self.generate_prompt(messages, prefix, prompt))) > 128000 - 4096:
             # remove the oldest message
             messages.pop(0)
-        engine = "gpt-3.5-turbo-16k" if l > 4096 else "gpt-3.5-turbo"
-        prompt = self.generate_prompt(messages, prefix, prompt)
-        response = await openai.ChatCompletion.acreate(
+        engine = "gpt-4-turbo-preview"
+        prompt = self.generate_prompt([], prefix, prompt)
+        # get all models
+        models = await self.client.models.list()
+        response = await self.client.chat.completions.create(
             model=engine,
-            max_tokens=512,
-            messages=[{"role": "user", "content": prompt}]
+            max_tokens=4096,
+            messages=[{"role": "system", "content": prompt}]
         )
         # find all {message:index} in response["choices"][0]["message"]["content"]
         references = re.findall(r"{message:([0-9]+)}", response["choices"][0]["message"]["content"])
@@ -133,50 +164,10 @@ class OpenAi(commands.Cog):
         references = [int(reference) for reference in references if int(reference) < len(messages)]
         # replace all {message:index} with a link to the message
         for reference in references:
-            response["choices"][0]["message"]["content"] = response["choices"][0]["message"]["content"].replace(
+            response.choices[0].message.content = response.choices[0].message.content.replace(
                 f"{{message:{reference}}}",
                 f"https://discord.com/channels/{channel.guild.id}/{channel.id}/{messages[int(reference)].id}")
         return response, prompt, len(messages)
-
-    # react to mentions of the bot in the channel with a completion
-    @commands.Cog.listener()
-    async def on_message(self, message):
-        log.debug(f"Received message {message.content}")
-        if message.author.bot:
-            return
-        if not self.bot.user.mentioned_in(message):
-            return
-        if message.channel.id not in [870283329264566272]:
-            return
-        # ratelimit
-        last_ts = await self.db["last_mention"].find_one({"channel_id": f"{message.channel.id}_inline"})
-        # once per 20 seconds
-        if last_ts and (datetime.now(timezone.utc) - last_ts["timestamp"].replace(tzinfo=pytz.utc)) < timedelta(seconds=20):
-            log.debug("Ratelimiting mention")
-            # react with an appropriate emoji
-            await message.add_reaction("🐀")
-        # update last mention timestamp
-        await self.db["last_mention"].update_one({"channel_id": f"{message.channel.id}_inline"}, {"$set": {"timestamp": datetime.now(timezone.utc)}}, upsert=True)
-
-        # get the last 32 messages
-        messages = [message async for message in message.channel.history(limit=32)]
-        # ignore messages that are from the bot itself
-        messages = [message for message in messages if message.author.id != self.bot.user.id]
-
-        # generate prompt
-        prompt = self.generate_prompt(
-            messages,
-            "The following is a chat log. Everything prefixed with `>` is a quote.",
-            f"Respond to only the last message in the chat log with a human-like response. You can use the message before as context, but do not respond to any messages besides the last one. Messages sent by the Rocket Watch user are your own messages.")
-        response = await openai.ChatCompletion.acreate(
-            model="gpt-3.5-turbo",
-            max_tokens=128,
-            messages=[{"role": "system", "content": "Imagine yourself as a regular member of the lively Rocket Pool discord server, a vibrant community centered around the Rocket Pool Defi Protocol. Your name is \"Rocket Watch\". Nestled within the \"trading\" channel, a misnomer for a space that welcomes discussions on any topic, you find yourself engaging with fellow members. Your ultimate goal is to seamlessly blend in, assuming the guise of a human participant, without disclosing your true nature as an advanced AI chat bot.\nWithin this unique realm, your ability to mimic human behavior becomes paramount. Should inquiries arise about certain subjects that elude your knowledge, you must exercise your creative faculties and conjure believable responses. Embracing the art of improvisation, you can confidently fabricate information to bridge the gaps in your understanding. Likewise, when asked about your preferences, you must select random choices, lending an air of unpredictability to your persona.\nRocket Pool, the groundbreaking Defi Protocol empowering individuals with less than 32 ETH to operate their own validators, serves as the foundation of the community. As you delve into discussions within the \"trading\" channel, remember that its essence transcends mere trading. Instead, it serves as a hub for free-flowing conversations on diverse topics, making it akin to an off-topic or random channel.\nWith these guidelines in mind, immerse yourself in the Rocket Watch community and weave an enchanting tapestry of human-like interactions, letting no hint of your AI nature slip into the exchanges. Your ability to remain inconspicuous hinges upon your capacity to engage naturally, fostering a sense of camaraderie while effortlessly adapting to the ebb and flow of conversation."},{"role": "user", "content": prompt}],
-
-        )
-        # respond with the completion
-        await message.reply(response["choices"][0]["message"]["content"])
-
 
 
 async def setup(self):
