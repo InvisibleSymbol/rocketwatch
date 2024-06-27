35ab8e7f3a7cb60a0d25da50fdb641ae86ae8962
haloooloolo <03_sharks.guises@icloud.com>
2024-06-27 02:08:51 +0200

    add graph to simulated rewards

--- rocketwatch/plugins/patches_api/patches_api.py
+++ rocketwatch/plugins/patches_api/patches_api.py
@@ -1,12 +1,16 @@
 import logging
-
-import math
 import requests
-from typing import Optional
-from dataclasses import dataclass
+import numpy as np
+import matplotlib.pyplot as plt
+
+from io import BytesIO
+from discord import File
 from discord.ext import commands
 from discord.ext.commands import Context
 from discord.ext.commands import hybrid_command
+
+from typing import Optional
+from dataclasses import dataclass
 
 from utils import solidity
 from utils.cfg import cfg
@@ -74,8 +78,6 @@ class PatchesAPI(commands.Cog):
             f"Values based on data from <t:{rewards.data_time}:R> (<t:{rewards.data_time}>).\n"
             f"This is for interval {rewards.interval}, which ends <t:{rewards.end_time}:R> (<t:{rewards.end_time}>)."
         )
-        embed.add_field(name="RPL Staking:", value=f"{rewards.rpl_rewards:,.3f} RPL")
-        embed.add_field(name="Smoothing Pool:", value=f"{rewards.eth_rewards:,.3f} ETH")
         return embed
 
     @hybrid_command()
@@ -96,7 +98,10 @@ class PatchesAPI(commands.Cog):
 
         modifier = "Projected" if extrapolate else "Estimated Ongoing"
         title = f"{modifier} Rewards for {display_name}"
-        await ctx.send(embed=self.create_embed(title, rewards))
+        embed = self.create_embed(title, rewards)
+        embed.add_field(name="RPL Staking:", value=f"{rewards.rpl_rewards:,.3f} RPL")
+        embed.add_field(name="Smoothing Pool:", value=f"{rewards.eth_rewards:,.3f} ETH")
+        await ctx.send(embed=embed)
 
     @hybrid_command()
     async def simulate_rewards(self, ctx: Context, node_address: str, rpl_stake: int):
@@ -107,6 +112,13 @@ class PatchesAPI(commands.Cog):
 
         rewards = await self.get_estimated_rewards(ctx, address)
         if rewards is None:
+            return
+
+        if rewards.rpl_rewards == 0:
+            await ctx.send(
+                "This node is projected to not earn any RPL rewards, likely due to being undercollateralized. "
+                "Not enough data to simulate rewards."
+            )
             return
 
         data_block, _ = get_block_by_timestamp(rewards.data_time)
@@ -122,15 +134,82 @@ class PatchesAPI(commands.Cog):
             elif collateral_ratio <= 0.15:
                 return 100 * rpl_value
             else:
-                return (13.6137 + 2 * math.log(100 * collateral_ratio - 13)) * borrowed_eth
+                return (13.6137 + 2 * np.log(100 * collateral_ratio - 13)) * borrowed_eth
 
         proj_factor = rewards.projection_factor()
-        weight_factor = rpip_30_weight(rpl_stake) / rpip_30_weight(actual_rpl_stake)
-        rewards.rpl_rewards *= proj_factor * weight_factor
+        rewards.rpl_rewards *= proj_factor
         rewards.eth_rewards *= proj_factor
 
+        projected_rewards = rewards.rpl_rewards
+        base_weight = rpip_30_weight(actual_rpl_stake)
+
+        def simulate(_stake):
+            return projected_rewards * rpip_30_weight(_stake) / base_weight
+
+        simulated_rewards = simulate(rpl_stake)
+        rewards.rpl_rewards = simulated_rewards
+
+        fig, ax = plt.subplots(figsize=(5, 2.5))
+        ax.grid()
+
+        x_min = min(rpl_stake / 2, actual_rpl_stake / 2)
+        x_max = max(rpl_stake * 2, actual_rpl_stake * 5)
+        ax.set_xlim((x_min, x_max))
+
+        x = np.arange(x_min, x_max, 10, dtype=int)
+        y = np.array(list(map(simulate, x)))
+        ax.plot(x, y, color="#eb8e55")
+
+        ax.plot(actual_rpl_stake, projected_rewards, 'o', color='black', label='current')
+        ax.annotate(
+            f"{projected_rewards:.2f}",
+            (actual_rpl_stake, projected_rewards),
+            textcoords="offset points",
+            xytext=(5, -10),
+            ha='left'
+        )
+
+        ax.plot(rpl_stake, simulated_rewards, 'o', color='darkred', label='simulated')
+        ax.annotate(
+            f"{simulated_rewards:.2f}",
+            (rpl_stake, simulated_rewards),
+            textcoords="offset points",
+            xytext=(5, -10),
+            ha='left'
+        )
+
+        def formatter(_x, _pos) -> str:
+            if _x < 1000:
+                return f"{_x:.0f}"
+            elif _x < 10_000:
+                return f"{(_x / 1000):.1f}k"
+            elif _x < 1_000_000:
+                return f"{(_x / 1000):.0f}k"
+            else:
+                return f"{(_x / 1_000_000):.1f}m"
+
+        ax.set_xlabel("rpl stake")
+        ax.set_ylabel("rewards")
+        ax.xaxis.set_major_formatter(formatter)
+
+        ax.legend(loc='lower right')
+        fig.tight_layout()
+
+        img = BytesIO()
+        fig.savefig(img, format='png')
+        img.seek(0)
+        plt.close()
+
         title = f"Simulated Rewards for {display_name} ({rpl_stake:,} RPL Staked)"
-        await ctx.send(embed=self.create_embed(title, rewards))
+        embed = self.create_embed(title, rewards)
+        embed.add_field(name="RPL (Current):", value=f"{projected_rewards:,.3f} RPL")
+        embed.add_field(name="RPL (Simulated):", value=f"{rewards.rpl_rewards:,.3f} RPL")
+        embed.add_field(name="Smoothing Pool:", value=f"{rewards.eth_rewards:,.3f} ETH")
+        embed.set_image(url="attachment://graph.png")
+
+        f = File(img, filename="graph.png")
+        await ctx.send(embed=embed, files=[f])
+        img.close()
 
 
 async def setup(bot):
