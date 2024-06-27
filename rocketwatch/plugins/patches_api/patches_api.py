bebde9e51f92937d00ba8be7931e05f40c92bd77
haloooloolo <03_sharks.guises@icloud.com>
2024-06-27 16:36:46 +0200

    add ability to simulate number of minipools

--- rocketwatch/plugins/patches_api/patches_api.py
+++ rocketwatch/plugins/patches_api/patches_api.py
@@ -102,7 +102,14 @@ class PatchesAPI(commands.Cog):
         await ctx.send(embed=embed)
 
     @hybrid_command()
-    async def simulate_rewards(self, ctx: Context, node_address: str, rpl_stake: int):
+    async def simulate_rewards(
+            self,
+            ctx: Context,
+            node_address: str,
+            rpl_stake: int,
+            num_leb8: int = 0,
+            num_eb16: int = 0
+    ):
         await ctx.defer(ephemeral=True)
         display_name, address = await resolve_ens(ctx, node_address)
         if display_name is None:
@@ -113,7 +120,7 @@ class PatchesAPI(commands.Cog):
             return
 
         rpl_ratio = solidity.to_float(rp.call("rocketNetworkPrices.getRPLPrice"))
-        borrowed_eth = solidity.to_float(rp.call("rocketNodeStaking.getNodeETHMatched", address))
+        actual_borrowed_eth = solidity.to_float(rp.call("rocketNodeStaking.getNodeETHMatched", address))
         actual_rpl_stake = solidity.to_float(rp.call("rocketNodeStaking.getNodeRPLStake", address))
 
         inflation_rate: int = rp.call("rocketTokenRPL.getInflationIntervalRate")
@@ -126,27 +133,21 @@ class PatchesAPI(commands.Cog):
             period_inflation = solidity.to_int(period_inflation * inflation_rate)
         period_inflation -= total_supply
 
-        def rpip_30_weight(_stake: float) -> float:
+        def rpip_30_weight(_stake: float, _borrowed_eth: float) -> float:
             rpl_value = _stake * rpl_ratio
-            collateral_ratio = rpl_value / borrowed_eth
+            collateral_ratio = rpl_value / _borrowed_eth
             if collateral_ratio < 0.1:
                 return 0.0
             elif collateral_ratio <= 0.15:
                 return 100 * rpl_value
             else:
-                return (13.6137 + 2 * np.log(100 * collateral_ratio - 13)) * borrowed_eth
+                return (13.6137 + 2 * np.log(100 * collateral_ratio - 13)) * _borrowed_eth
 
-        def rewards_at(_stake: float) -> float:
-            weight = rpip_30_weight(_stake)
-            base_weight = rpip_30_weight(actual_rpl_stake)
+        def rewards_at(_stake: float, _borrowed_eth: float) -> float:
+            weight = rpip_30_weight(_stake, _borrowed_eth)
+            base_weight = rpip_30_weight(actual_rpl_stake, _borrowed_eth)
             new_system_weight = rewards.system_weight + weight - base_weight
             return solidity.to_float(0.7 * period_inflation * weight / new_system_weight)
-
-        projected_rewards = rewards_at(actual_rpl_stake)
-        simulated_rewards = rewards_at(rpl_stake)
-
-        proj_factor = (rewards.end_time - rewards.start_time) / (rewards.data_time - rewards.start_time)
-        eth_rewards = rewards.eth_rewards * proj_factor
 
         fig, ax = plt.subplots(figsize=(5, 2.5))
         ax.grid()
@@ -155,27 +156,37 @@ class PatchesAPI(commands.Cog):
         x_max = max(rpl_stake * 2, actual_rpl_stake * 5)
         ax.set_xlim((x_min, x_max))
 
-        x = np.arange(x_min, x_max, 10, dtype=int)
-        y = np.array(list(map(rewards_at, x)))
-        ax.plot(x, y, color="#eb8e55")
+        def draw_reward_curve(_color: str, _line_style: str, _borrowed_eth: float) -> None:
+            x = np.arange(x_min, x_max, 10, dtype=int)
+            y = np.array([rewards_at(x, _borrowed_eth) for x in x])
+            ax.plot(x, y, color=_color, linestyle=_line_style)
 
-        ax.plot(actual_rpl_stake, projected_rewards, 'o', color='black', label='current')
-        ax.annotate(
-            f"{projected_rewards:.2f}",
-            (actual_rpl_stake, projected_rewards),
-            textcoords="offset points",
-            xytext=(5, -10 if projected_rewards > 0 else 5),
-            ha='left'
-        )
+            projected_rewards = rewards_at(actual_rpl_stake, _borrowed_eth)
+            simulated_rewards = rewards_at(rpl_stake, _borrowed_eth)
 
-        ax.plot(rpl_stake, simulated_rewards, 'o', color='darkred', label='simulated')
-        ax.annotate(
-            f"{simulated_rewards:.2f}",
-            (rpl_stake, simulated_rewards),
-            textcoords="offset points",
-            xytext=(5, -10 if simulated_rewards > 0 else 5),
-            ha='left'
-        )
+            ax.plot(actual_rpl_stake, projected_rewards, 'o', color="#eb8e55", label="current")
+            ax.annotate(
+                f"{projected_rewards:.2f}",
+                (actual_rpl_stake, projected_rewards),
+                textcoords="offset points",
+                xytext=(5, -10 if projected_rewards > 0 else 5),
+                ha="left"
+            )
+
+            ax.plot(rpl_stake, simulated_rewards, 'o', color="darkred", label="simulated")
+            ax.annotate(
+                f"{simulated_rewards:.2f}",
+                (rpl_stake, simulated_rewards),
+                textcoords="offset points",
+                xytext=(5, -10 if simulated_rewards > 0 else 5),
+                ha="left"
+            )
+
+        draw_reward_curve("#eb8e55", "solid", actual_borrowed_eth)
+
+        borrowed_eth = (24 * num_leb8) + (16 * num_eb16)
+        if borrowed_eth > 0:
+            draw_reward_curve("darkred", "dashed", borrowed_eth)
 
         def formatter(_x, _pos) -> str:
             if _x < 1000:
@@ -191,7 +202,9 @@ class PatchesAPI(commands.Cog):
         ax.set_ylabel("rewards")
         ax.xaxis.set_major_formatter(formatter)
 
-        ax.legend(loc='lower right')
+        handles, labels = ax.get_legend_handles_labels()
+        by_label = dict(zip(labels, handles))
+        plt.legend(by_label.values(), by_label.keys(), loc='lower right')
         fig.tight_layout()
 
         img = BytesIO()
@@ -199,11 +212,14 @@ class PatchesAPI(commands.Cog):
         img.seek(0)
         plt.close()
 
-        title = f"Simulated Rewards for {display_name} ({rpl_stake:,} RPL Staked)"
+        sim_info = f"{rpl_stake:,} RPL"
+        if num_leb8 > 0:
+            sim_info += f", {num_leb8} x 8 ETH"
+        if num_eb16 > 0:
+            sim_info += f", {num_eb16} x 16 ETH"
+
+        title = f"Simulated RPL Rewards for {display_name} ({sim_info})"
         embed = self.create_embed(title, rewards)
-        embed.add_field(name="RPL (Current):", value=f"{projected_rewards:,.3f} RPL")
-        embed.add_field(name="RPL (Simulated):", value=f"{simulated_rewards:,.3f} RPL")
-        embed.add_field(name="Smoothing Pool:", value=f"{eth_rewards:,.3f} ETH")
         embed.set_image(url="attachment://graph.png")
 
         f = File(img, filename="graph.png")
