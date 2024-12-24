fe07deb1ddd5242c092eef70c06d2eedfd8a0626
InvisibleSymbol <34715248+InvisibleSymbol@users.noreply.github.com>
2024-12-24 19:50:41 +0100

    chore(everything): ye

--- rocketwatch/plugins/minipool_distribution/minipool_distribution.py
+++ rocketwatch/plugins/minipool_distribution/minipool_distribution.py
@@ -106,7 +106,7 @@ class MinipoolDistribution(commands.Cog):
         e.set_image(url="attachment://graph.png")
         f = File(img, filename="graph.png")
         percentile_strings = [f"{x[0]}th percentile: {p.no('minipool', x[1])} per node" for x in
-                              get_percentiles([50, 75, 90, 99], counts)]
+                              get_percentiles([50, 75, 90, 99], counts) if x[1]]
         percentile_strings.append(f"Max: {distribution[-1][0]} minipools per node")
         percentile_strings.append(f"Total: {p.no('minipool', sum(counts))}")
         e.set_footer(text="\n".join(percentile_strings))
