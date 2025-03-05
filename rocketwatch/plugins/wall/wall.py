50b49087a9b8d245ce8507215875e693370a3c26
haloooloolo <03_sharks.guises@icloud.com>
2025-03-05 22:38:04 +0000

    liquidity source selection

--- rocketwatch/plugins/wall/wall.py
+++ rocketwatch/plugins/wall/wall.py
@@ -1,6 +1,6 @@
 import asyncio
 from io import BytesIO
-from typing import cast
+from typing import cast, Literal
 
 from discord import File
 from discord.ext import commands
@@ -47,7 +47,7 @@ class Wall(commands.Cog):
         ]
 
     @staticmethod
-    def _get_cex_data(liquidity: dict[CEX, Liquidity], x: np.ndarray) -> list[tuple[np.ndarray, str, str]]:
+    def _get_cex_data(liquidity: dict[CEX, Liquidity], x: np.ndarray, max_unique: int) -> list[tuple[np.ndarray, str, str]]:
         depth: dict[CEX, np.ndarray] = {}
         for cex, liq in liquidity.items():
             depth[cex] = np.array(list(map(liq.depth_at, x)))
@@ -58,16 +58,16 @@ class Wall(commands.Cog):
         exchanges = list(sorted(depth, key=get_range_liquidity, reverse=True))
         ret = []
 
-        if len(exchanges) > 3:
-            y = np.sum([depth[cex] for cex in exchanges[3:]], axis=0)
-            ret.append((y, "Other", "#555555"))
-
-        for exchange in reversed(exchanges[:3]):
+        for exchange in exchanges[:max_unique]:
             ret.append((depth[exchange], str(exchange), exchange.color))
+
+        if len(exchanges) > max_unique:
+            y = np.sum([depth[cex] for cex in exchanges[max_unique:]], axis=0)
+            ret.append((y, "Other", "#555555"))
 
         return ret
 
-    def _get_dex_data(self, x: np.ndarray, rpl_usd: float) -> list[tuple[np.ndarray, str, str]]:
+    def _get_dex_data(self, x: np.ndarray, rpl_usd: float, max_unique: int) -> list[tuple[np.ndarray, str, str]]:
         depth: dict[DEX, np.ndarray] = {}
         liquidity: dict[DEX, float] = {}
         for dex in self.dex:
@@ -89,82 +89,79 @@ class Wall(commands.Cog):
         exchanges = list(sorted(depth, key=get_range_liquidity, reverse=True))
         ret = []
 
-        if len(exchanges) > 2:
-            y = np.sum([depth[cex] for cex in exchanges[2:]], axis=0)
-            ret.append((y, "Other", "#777777"))
-
-        for exchange in reversed(exchanges[:2]):
+        for exchange in exchanges[:max_unique]:
             ret.append((depth[exchange], str(exchange), exchange.color))
+
+        if len(exchanges) > max_unique:
+            y = np.sum([depth[cex] for cex in exchanges[max_unique:]], axis=0)
+            ret.append((y, "Other", "#777777"))
 
         return ret
 
     @staticmethod
-    def _plot_data(x, rpl_usd, cex_data, dex_data) -> figure.Figure:
-        cex_labels, cex_colors = [], []
+    def _plot_data(
+            x: np.ndarray,
+            rpl_usd: float,
+            cex_data: list[tuple[np.ndarray, str, str]],
+            dex_data: list[tuple[np.ndarray, str, str]]
+    ) -> figure.Figure:
         y = []
-
-        for y_values, label, color in cex_data:
-            y.append(y_values)
-            cex_labels.append(label)
-            cex_colors.append(color)
-
-        dex_labels, dex_colors = [], []
-        for y_values, label, color in dex_data:
-            y.append(y_values)
-            dex_labels.append(label)
-            dex_colors.append(color)
+        colors = []
 
         fig, ax = plt.subplots(figsize=(10, 5))
         ax.set_facecolor("#f8f9fa")
-
         ax.set_title("RPL Market Depth", fontsize=14, fontweight='bold')
 
         ax.minorticks_on()
         ax.grid(True, linestyle="--", linewidth=0.5, alpha=0.5)
         ax.grid(True, which='minor', linestyle=':', linewidth=0.3, alpha=0.5)
 
-        ax.stackplot(x, np.array(y), colors=(cex_colors + dex_colors), edgecolor="black", linewidth=0.3)
         ax.axvline(rpl_usd, color="black", linestyle="--", linewidth=1)
 
         ax.xaxis.set_major_formatter(ticker.StrMethodFormatter('${x:,.2f}'))
         ax.yaxis.set_major_formatter(ticker.StrMethodFormatter('${x:,.0f}'))
 
+        y_offset = 0
+
+        def add_data(_data: list[tuple[np.ndarray, str, str]], _legend_title: Optional[str]) -> None:
+            labels, handles = [], []
+            for y_values, label, color in _data:
+                y.append(y_values)
+                labels.append(label)
+                colors.append(color)
+                handles.append(plt.Rectangle((0, 0), 1, 1, color=color))
+
+            legend = ax.legend(
+                handles,
+                labels,
+                title=_legend_title,
+                fontsize=10,
+                title_fontsize=10,
+                loc="upper left",
+                bbox_to_anchor=(0, 1 - y_offset)
+            )
+            ax.add_artist(legend)
+
+        if dex_data:
+            title = "DEX" if cex_data else None
+            add_data(dex_data, title)
+            y_offset += 0.085 + 0.055 * len(dex_data)
+
+        if cex_data:
+            title = "CEX" if dex_data else None
+            add_data(cex_data, title)
+
+        ax.stackplot(x, np.array(y[::-1]), colors=colors[::-1], edgecolor="black", linewidth=0.3)
+
         x_ticks = ax.get_xticks()
         x_ticks = [t for t in x_ticks if abs(t - rpl_usd) > (x[-1] - x[0]) / 20] + [rpl_usd]
         ax.set_xticks(x_ticks)
-
         ax.set_xlim((x[0], x[-1]))
-
-        handles = [plt.Rectangle((0, 0), 1, 1, color=color) for color in dex_colors]
-        legend = ax.legend(
-            handles[::-1],
-            dex_labels[::-1],
-            title="DEX",
-            fontsize=10,
-            title_fontsize=10,
-            loc="upper left",
-            bbox_to_anchor=(0, 1)
-        )
-        ax.add_artist(legend)
-
-        y_offset = 0.1 + 0.055 * len(handles)
-
-        handles = [plt.Rectangle((0, 0), 1, 1, color=color) for color in cex_colors]
-        legend = ax.legend(
-            handles[::-1],
-            cex_labels[::-1],
-            title="CEX",
-            fontsize=10,
-            title_fontsize=10,
-            loc="upper left",
-            bbox_to_anchor=(0, 1 - y_offset)
-        )
-        ax.add_artist(legend)
 
         return fig
 
     @hybrid_command()
-    async def wall(self, ctx: Context):
+    async def wall(self, ctx: Context, exchanges: Literal["All", "CEX", "DEX"] = "All"):
         """Show the current RPL market depth across exchanges."""
         await ctx.defer(ephemeral=is_hidden_weak(ctx))
         embed = Embed()
@@ -189,20 +186,35 @@ class Wall(commands.Cog):
         rpl_usd = float(np.mean([liq.price for liq in cex_liquidity.values()]))
         x = np.arange(0, 5 * rpl_usd, 0.01)
 
-        cex_data = self._get_cex_data(cex_liquidity, x)
-        dex_data = self._get_dex_data(x, rpl_usd)
-        fig = self._plot_data(x, rpl_usd, cex_data, dex_data)
+        sources = []
+        cex_data, dex_data = [], []
+        total_liquidity = 0
+
+        if exchanges != "CEX":
+            max_unique = 5 if (exchanges == "DEX") else 2
+            dex_data = self._get_dex_data(x, rpl_usd, max_unique)
+            sources.append(f"{len(self.dex)} DEX")
+            total_liquidity += sum(y[0] + y[-1] for y, _, _ in dex_data)
+
+        if exchanges != "DEX":
+            max_unique = 5 if (exchanges == "CEX") else 3
+            cex_data = self._get_cex_data(cex_liquidity, x, max_unique)
+            sources.append(f"{len(self.cex)} CEX")
+            total_liquidity += sum(y[0] + y[-1] for y, _, _ in cex_data)
 
         embed.add_field(name="Current Price", value=f"${rpl_usd:,.2f}")
-        embed.add_field(name="Liquidity Sources", value=f"{len(self.cex)} CEX, {len(self.dex)} DEX")
+        embed.add_field(name="Observed Liquidity", value=f"${total_liquidity:,.0f}")
+        embed.add_field(name="Sources", value=", ".join(sources))
 
         buffer = BytesIO()
+        fig = self._plot_data(x, rpl_usd, cex_data, dex_data)
         fig.savefig(buffer, format="png")
         buffer.seek(0)
 
         file_name = "wall.png"
         embed.set_image(url=f"attachment://{file_name}")
         await ctx.send(embed=embed, files=[File(buffer, file_name)])
+
 
 async def setup(bot):
     await bot.add_cog(Wall(bot))
