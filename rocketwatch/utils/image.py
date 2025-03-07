8eac00a747dbc4febeb1004b854a48aed2bc143c
haloooloolo <03_sharks.guises@icloud.com>
2025-03-07 23:27:36 +0000

    more tweaks

--- rocketwatch/utils/image.py
+++ rocketwatch/utils/image.py
@@ -1,5 +1,6 @@
 from io import BytesIO
-from typing import Optional
+from typing import Optional, Literal
+from functools import cache
 
 from PIL import ImageFont
 from PIL import Image as PillowImage
@@ -24,48 +25,57 @@ class ImageCanvas(ImageDraw):
         p_img = PillowImage.new('RGB', (width, height), color=bg_color)
         super().__init__(p_img)
         self.image = Image(p_img)
-        self._fonts_cache: dict[int, ImageFont] = {}
 
     def progress_bar(
             self,
-            xy: tuple[int, int],
-            size: tuple[int, int],
+            xy: tuple[float, float],
+            size: tuple[float, float],
             progress: float,
-            primary: Color = (211, 211, 211),
+            primary: Color,
             secondary : Color = (15, 15, 15)
     ) -> None:
         x, y = xy
         height, width = size
-        # Draw the background
-        self.rectangle((x + (height / 2), y, x + width + (height / 2), y + height), fill=secondary, width=10)
-        self.ellipse((x + width, y, x + height + width, y + height), fill=secondary)
-        self.ellipse((x, y, x + height, y + height), fill=secondary)
-        width = int(width * progress)
-        # Draw the part of the progress bar that is actually filled
-        self.rectangle((x + (height / 2), y, x + width + (height / 2), y + height), fill=primary, width=10)
-        self.ellipse((x + width, y, x + height + width, y + height), fill=primary)
-        self.ellipse((x, y, x + height, y + height), fill=primary)
+        if width <= 2 * height:
+            raise ValueError("Progress bar width must be at least twice its height")
 
-    def _get_font(self, font_size: int) -> ImageFont:
-        if font_size not in self._fonts_cache:
-            font = ImageFont.truetype("DejaVuSans.ttf", font_size)
-            self._fonts_cache[font_size] = font
-        return self._fonts_cache[font_size]
+        # background
+        radius = height / 2
+        x0 = x + radius
+        x1 = x + width - radius
+        self.circle((x0, y + radius), radius, fill=secondary)
+        self.rectangle((x0, y, x1, y + height), fill=secondary)
+        self.circle((x1, y + radius), radius, fill=secondary)
+
+        # fill
+        x1 = x + round(progress * width) - radius
+        if progress > 0:
+            self.circle((x0, y + radius), radius, fill=primary)
+        if x1 >= x0 + radius:
+            self.rectangle((x0, y, x1, y + height), fill=primary)
+        if progress == 1:
+            self.circle((x1, y + radius), radius, fill=primary)
+
+    @cache
+    def _get_font(self, variant: str, size: float) -> ImageFont:
+        return ImageFont.truetype(f"fonts/Inter-{variant}.ttf", size)
 
     def dynamic_text(
             self,
-            xy: tuple[int, int],
+            xy: tuple[float, float],
             text: str,
-            font_size: int,
-            color: Color = (211, 211, 211),
-            max_width: Optional[int] = None,
+            font_size: float,
+            font_variant: Literal["Regular", "Bold", "Black"] = "Regular",
+            color: Color = (255, 255, 255),
+            max_width: Optional[float] = None,
             anchor: str = "lt"
-    ):
-        font = self._get_font(font_size)
+    ) -> None:
+        font = self._get_font(font_variant, font_size)
         if max_width is not None:
             # cut off the text if it's too long
-            while font.getbbox(text)[2] > max_width and text:
+            while text and (font.getbbox(text)[2] > max_width):
                 text = text[:-1]
                 # replace last character with an ellipsis
                 text = f"{text[:-1]}…"
+
         self.text(xy, text, font=font, fill=color, anchor=anchor)
