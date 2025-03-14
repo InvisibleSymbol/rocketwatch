34e4bb5f0055cfda2b91bf82e92d9ee96ce1d68f
InvisibleSymbol <34715248+InvisibleSymbol@users.noreply.github.com>
2025-03-14 19:43:17 +0100

    Revert "add retry to proposals"
    This reverts commit 615867512b994e985a776768ad93749c30179ddb.

--- rocketwatch/utils/retry.py
+++ rocketwatch/utils/retry.py
@@ -1,22 +1,5 @@
-from retry_async.api import (
-    retry as __retry,
-    EXCEPTIONS
-)
-from typing import Callable, Any
+from retry_async import retry as __retry
+from functools import partial
 
-
-def retry(
-    exceptions: EXCEPTIONS = Exception,
-    *,
-    tries: int = -1,
-    delay: float = 0
-) -> Callable[..., Any]:
-    return __retry(exceptions, is_async=False, tries=tries, delay=delay)
-
-def retry_async(
-    exceptions: EXCEPTIONS = Exception,
-    *,
-    tries: int = -1,
-    delay: float = 0
-) -> Callable[..., Any]:
-    return __retry(exceptions, is_async=True, tries=tries, delay=delay)
+retry = partial(__retry, is_async=False)
+retry_async = partial(__retry, is_async=True)
