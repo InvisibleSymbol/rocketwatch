03cfb995a38b08387d74082fc9e0c0dd6c519f11
haloooloolo <03_sharks.guises@icloud.com>
2025-03-16 20:35:36 +0100

    use multicall for queue stuff

--- rocketwatch/plugins/deposit_pool/deposit_pool.py
+++ rocketwatch/plugins/deposit_pool/deposit_pool.py
@@ -6,7 +6,7 @@ from discord.ext.commands import hybrid_command
 from motor.motor_asyncio import AsyncIOMotorClient
 
 from rocketwatch import RocketWatch
-from plugins.queue import queue
+from plugins.queue.queue import Queue
 from utils.status import StatusPlugin
 from utils import solidity
 from utils.cfg import cfg
@@ -23,58 +23,66 @@ class DepositPool(StatusPlugin):
         super().__init__(bot)
         self.db = AsyncIOMotorClient(cfg["mongodb_uri"]).rocketwatch
 
+    @staticmethod
+    def get_deposit_pool_stats() -> Embed:
+        multicall: dict[str, int] = {
+            res.function_name: res.results[0] for res in rp.multicall.aggregate([
+                rp.get_contract_by_name("rocketDepositPool").functions.getBalance(),
+                rp.get_contract_by_name("rocketDAOProtocolSettingsDeposit").functions.getMaximumDepositPoolSize(),
+                rp.get_contract_by_name("rocketDepositPool").functions.getMaximumDepositAmount(),
+                rp.get_contract_by_name("rocketMinipoolQueue").functions.getTotalLength(),
+            ]).results
+        }
+
+        dp_balance = solidity.to_float(multicall["getBalance"])
+        deposit_cap = solidity.to_int(multicall["getMaximumDepositPoolSize"])
+
+        if deposit_cap - dp_balance < 0.01:
+            dp_status = "Deposit Pool Cap Reached!"
+        else:
+            fill_perc = dp_balance / deposit_cap
+            free_capacity = solidity.to_float(multicall["getMaximumDepositAmount"])
+            dp_status = f"Buffer {fill_perc:.2%} full. Enough space for **{free_capacity:.2f}** more ETH."
+
+        embed = Embed(title="Deposit Pool Stats")
+        embed.add_field(name="Current Size", value=f"{dp_balance:.2f} ETH")
+        embed.add_field(name="Maximum Size", value=f"{deposit_cap:,} ETH")
+        embed.add_field(name="Status", value=dp_status, inline=False)
+
+        queue_length = multicall["getTotalLength"]
+        if queue_length > 0:
+            embed.description = Queue.get_minipool_queue(limit=5).description
+            queue_capacity = max(queue_length * 31 - dp_balance, 0.0)
+            embed.description += f"\nNeed **{queue_capacity:.2f}** more ETH to dequeue all minipools."
+        else:
+            lines = []
+            if (num_leb8 := dp_balance // 24) > 0:
+                lines.append(f"**`{num_leb8:>4.0f}`** 8 ETH minipools (24 ETH from DP)")
+            if (num_credit := dp_balance // 32) > 0:
+                lines.append(f"**`{num_credit:>4.0f}`** credit minipools (32 ETH from DP)")
+
+            if lines:
+                embed.add_field(name="Enough For", value="\n".join(lines), inline=False)
+
+        return embed
+
     @hybrid_command()
     async def dp(self, ctx: Context):
         """Deposit Pool Stats"""
         await self.deposit_pool(ctx)
 
-    @staticmethod
-    async def get_status_message() -> Embed:
-        embed = DepositPool.get_dp()
-        embed.title = ":rocket: Live Deposit Pool Status"
-        return embed
-
     @hybrid_command()
     async def deposit_pool(self, ctx: Context):
         """Deposit Pool Stats"""
         await ctx.defer(ephemeral=is_hidden_weak(ctx))
-        embed = self.get_dp()
+        embed = self.get_deposit_pool_stats()
         await ctx.send(embed=embed)
 
     @staticmethod
-    def get_dp() -> Embed:
-        e = Embed(title="Deposit Pool Stats")
-
-        deposit_pool = solidity.to_float(rp.call("rocketDepositPool.getBalance"))
-        e.add_field(name="Current Size:", value=f"{humanize.intcomma(round(deposit_pool, 2))} ETH")
-
-        deposit_cap = solidity.to_int(rp.call("rocketDAOProtocolSettingsDeposit.getMaximumDepositPoolSize"))
-        e.add_field(name="Maximum Size:", value=f"{humanize.intcomma(deposit_cap)} ETH")
-
-        if deposit_cap - deposit_pool < 0.01:
-            e.add_field(name="Status:", value="Deposit Pool Cap Reached!", inline=False)
-        else:
-            percentage_filled = round(deposit_pool / deposit_cap * 100, 2)
-            free_capacity = solidity.to_float(rp.call("rocketDepositPool.getMaximumDepositAmount"))
-            free_capacity = round(free_capacity, 2)
-            e.add_field(name="Status:",
-                        value=f"Buffer {percentage_filled}% Full. Enough space for **{humanize.intcomma(free_capacity)}** more ETH",
-                        inline=False)
-
-        queue_length = rp.call("rocketMinipoolQueue.getTotalLength")
-        if queue_length > 0:
-            e.description = queue.get_queue(l=5)
-            e.description += f"\nNeed **{humanize.intcomma(max(round(queue_length * 31 - deposit_pool, 2), 0))}** more ETH to dequeue all minipools"
-        elif deposit_pool // 16 > 0:
-            e.add_field(name="Enough For:",
-                        value=f"**`{deposit_pool // 16:>4.0f}`** 16 ETH Minipools (16 ETH from DP)" +
-                              (
-                                  f"\n**`{deposit_pool // 24:>4.0f}`** 8 ETH Minipools (24 ETH from DP)" if deposit_pool // 24 > 0 else "") +
-                              (
-                                  f"\n**`{deposit_pool // 32:>4.0f}`** Credit Minipools (32 ETH from DP)" if deposit_pool // 32 > 0 else ""),
-                        inline=False)
-
-        return e
+    async def get_status_message() -> Embed:
+        embed = DepositPool.get_deposit_pool_stats()
+        embed.title = ":rocket: Live Deposit Pool Status"
+        return embed
 
     @hybrid_command()
     async def reth_extra_collateral(self, ctx: Context):
